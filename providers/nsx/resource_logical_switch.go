145dc84f474b827e227e08a648a6ff97665d59bc
Ben Dalling <benedict.dalling@sky.uk>
2016-07-08 17:40:32 +0100

    Work in progress.

--- providers/nsx/resource_logical_switch.go
+++ providers/nsx/resource_logical_switch.go
@@ -1,7 +1,12 @@
 package main
 
 import (
+    "errors"
+    "fmt"
+    "git.devops.int.ovp.bskyb.com/paas/gonsx/client"
+    "git.devops.int.ovp.bskyb.com/paas/gonsx/client/api/virtualwire"
     "github.com/hashicorp/terraform/helper/schema"
+    "log"
 )
 
 func resourceLogicalSwitch() *schema.Resource {
@@ -11,11 +16,46 @@ func resourceLogicalSwitch() *schema.Resource {
         Delete: resourceLogicalSwitchDelete,
 
         Schema: map[string]*schema.Schema{
+            "desc": {
+                Type:     schema.TypeString,
+                Required: true,
+                ForceNew: true,
+            },
+
+            "tenantid": {
+                Type:     schema.TypeString,
+                Required: true,
+                ForceNew: true,
+            },
+
+            "scopeid": {
+                Type:     schema.TypeString,
+                Required: true,
+                ForceNew: true,
+            },
         },
     }
 }
 
 func resourceLogicalSwitchCreate(d *schema.ResourceData, m interface{}) error {
+    nsxclient := m.(*client.NSXClient)
+
+    if v, ok := d.GetOk("desc"); ok {
+        desc := v.(string)
+        log.Printf("[DEBUG] Create Logical Switch - desc := ", desc)
+    } else {
+        return fmt.Errorf("desc argument is required")
+    }
+
+    create_api := virtualwire.NewCreate("test", "test desc", "test", "vdnscope-19")
+    nsxclient.Do(create_api)
+
+    if create_api.StatusCode() != 201 {
+        log.Printf(create_api.GetResponse())
+        return errors.New(create_api.GetResponse())
+    }
+
+    d.SetId(create_api.GetResponse())
     return nil
 }
 
