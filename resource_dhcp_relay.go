f5edb7f553c6d089a32f3b5509c8e4818f3d3054
Ben Dalling <benedict.dalling@sky.uk>
2016-07-13 17:20:00 +0100

    Added Read and Delete methods.

--- resource_dhcp_relay.go
+++ resource_dhcp_relay.go
@@ -132,9 +132,95 @@ func resourceDHCPRelayCreate(d *schema.ResourceData, m interface{}) error {
 }
 
 func resourceDHCPRelayRead(d *schema.ResourceData, m interface{}) error {
+        nsxclient := m.(*gonsx.NSXClient)
+        var edgeid, vnicindex string
+
+        // Gather the attributes for the resource.
+        if v, ok := d.GetOk("edgeid"); ok {
+                edgeid = v.(string)
+        } else {
+                return fmt.Errorf("edgeid argument is required")
+        }
+
+        if v, ok := d.GetOk("vnicindex"); ok {
+                vnicindex = v.(string)
+        } else {
+                return fmt.Errorf("vnicindex argument is required")
+        }
+
+        // Create the API, use it and check for errors.
+        log.Printf(fmt.Sprintf("[DEBUG] dhcprelay.getAllDhcpRelays(%s, %s)", edgeid, nsxclient))
+        currentDHCPRelay, err := getAllDhcpRelays(edgeid, nsxclient)
+
+        if err != nil {
+                return fmt.Errorf("Error:", err)
+        }
+
+        if !currentDHCPRelay.CheckByVnicIndex(vnicindex) {
+          d.SetId("")
+        }
+
 	return nil
 }
 
 func resourceDHCPRelayDelete(d *schema.ResourceData, m interface{}) error {
+        nsxclient := m.(*gonsx.NSXClient)
+        var edgeid, vnicindex string
+
+        // Gather the attributes for the resource.
+        if v, ok := d.GetOk("edgeid"); ok {
+                edgeid = v.(string)
+        } else {
+                return fmt.Errorf("edgeid argument is required")
+        }
+
+        if v, ok := d.GetOk("vnicindex"); ok {
+                vnicindex = v.(string)
+        } else {
+                return fmt.Errorf("vnicindex argument is required")
+        }
+
+        // Create the API, use it and check for errors.
+        log.Printf(fmt.Sprintf("[DEBUG] dhcprelay.getAllDhcpRelays(%s, %s)", edgeid, nsxclient))
+        currentDHCPRelay, err := getAllDhcpRelays(edgeid, nsxclient)
+
+        if err != nil {
+                return fmt.Errorf("Error:", err)
+        }
+
+        // Check to see if an entry with the vnicindex exists at all.  If
+        // not, assume it has been deleted manually and notify Terraform
+        // and exit gracefully.
+        if !currentDHCPRelay.CheckByVnicIndex(vnicindex) {
+          d.SetId("")
+          return nil
+        }
+
+	if currentDHCPRelay.CheckByVnicIndex(vnicindex) && (len(currentDHCPRelay.RelayAgents) == 1) {
+		delete_api := dhcprelay.NewDelete(edgeid)
+		err = nsxclient.Do(delete_api)
+		if err != nil {
+                        return fmt.Errorf("Error:", err)
+		} else {
+			log.Println("DHCP Relay agent deleted.")
+		}
+	} else {
+		// if we got more than one relay agents, then we have to call update after removing
+		// the entry we want to remove.
+		log.Println("There are other DHCP Relay agents, only removing single entry with update.")
+		newRelayAgentsList := currentDHCPRelay.RemoveByVnicIndex(vnicindex).RelayAgents
+
+		update_api := dhcprelay.NewUpdate(currentDHCPRelay.RelayServer.IpAddress, edgeid, newRelayAgentsList)
+		err = nsxclient.Do(update_api)
+
+		if err != nil {
+			return fmt.Errorf("Error:", err)
+		} else if update_api.StatusCode() != 204 {
+		        return fmt.Errorf(update_api.GetResponse())
+		} else {
+                        log.Printf("Updated DHCP Relay - %s", update_api.GetResponse())
+                }
+	}
+
 	return nil
 }
