9a79b38f869decff2956ac7140b78cdd54e68a05
Sandeep Sidhu <ssa52@ovp.bskyb.com>
2016-07-28 16:34:39 +0100

    Updating security policy changes

--- resource_security_policy_rules.go
+++ resource_security_policy_rules.go
@@ -64,7 +64,7 @@ func resourceSecurityPolicyRuleCreate(d *schema.ResourceData, m interface{}) err
 	}
 
 	if v, ok := d.GetOk("securitypolicyname"); ok {
-		name = v.(string)
+		securitypolicyname = v.(string)
 	} else {
 		return fmt.Errorf("securitypolicyname argument is required")
 	}
@@ -96,17 +96,27 @@ func resourceSecurityPolicyRuleCreate(d *schema.ResourceData, m interface{}) err
 		return fmt.Errorf("securitygroupids argument is required")
 	}
 
-
+	log.Print("Getting policy object to modify")
 	policyToModify, err := getSingleSecurityPolicy(securitypolicyname, nsxclient)
-
+	log.Printf("[DEBUG] - policyTOModify :%s", policyToModify)
 
 	if err != nil {
 		return err
 	}
 
+	existingAction := policyToModify.GetFirewallRuleByName(name)
+	if existingAction != nil {
+		return fmt.Errorf("Firewall rule with same name already exists in this security policy.")
+	}
+
 	log.Printf(fmt.Sprintf("[DEBUG] policyToModify.AddOutboundFirewallAction(%s, %s, %s, %s)",name, action, direction, securitygroupids))
-	policyToModify.AddOutboundFirewallAction(name, action, direction, securitygroupids)
+	modifyErr := policyToModify.AddOutboundFirewallAction(name, action, direction, securitygroupids)
+	if err != nil {
+		return fmt.Errorf("Error in adding the rule to policy object: %s", modifyErr)
+	}
+	log.Printf("[DEBUG] - policyTOModify :%s", policyToModify)
 	updateAPI := securitypolicy.NewUpdate(policyToModify.ObjectID, policyToModify)
+
 	err = nsxclient.Do(updateAPI)
 
 	if err != nil {
@@ -117,14 +127,92 @@ func resourceSecurityPolicyRuleCreate(d *schema.ResourceData, m interface{}) err
 		return fmt.Errorf("%s", updateAPI.ResponseObject())
 	}
 
-	d.SetId(policyToModify.VsmUUID)
+	newAction := policyToModify.GetFirewallRuleByName(name)
+	d.SetId(newAction.VsmUUID)
+	d.Set("VsmUUID", newAction.VsmUUID)
 	return resourceSecurityPolicyRuleRead(d, m)
 }
 
 func resourceSecurityPolicyRuleRead(d *schema.ResourceData, m interface{}) error {
+	nsxclient := m.(*gonsx.NSXClient)
+	var name string
+	var securitypolicyname string
+
+	if v, ok := d.GetOk("name"); ok {
+		name = v.(string)
+	} else {
+		return fmt.Errorf("name argument is required")
+	}
+	if v, ok := d.GetOk("securitypolicyname"); ok {
+		securitypolicyname = v.(string)
+	} else {
+		return fmt.Errorf("securitypolicyname argument is required")
+	}
+
+	policyToRead, err := getSingleSecurityPolicy(securitypolicyname, nsxclient)
+	log.Printf("[DEBUG] - policyToRead :%s", policyToRead)
+
+	if err != nil {
+		return err
+	}
+
+	existingAction := policyToRead.GetFirewallRuleByName(name)
+	id := existingAction.VsmUUID
+	log.Printf(fmt.Sprintf("[DEBUG] VsmUUID := %s", id))
+
+	// If the resource has been removed manually, notify Terraform of this fact.
+	if id == "" {
+		d.SetId("")
+	}
 	return nil
 }
 
 func resourceSecurityPolicyRuleDelete(d *schema.ResourceData, m interface{}) error {
+	nsxclient := m.(*gonsx.NSXClient)
+	var name string
+	var securityPolicyName string
+
+	if v, ok := d.GetOk("name"); ok {
+		name = v.(string)
+	} else {
+		return fmt.Errorf("name argument is required")
+	}
+
+	if v, ok := d.GetOk("securitypolicyname"); ok {
+		securityPolicyName = v.(string)
+	} else {
+		return fmt.Errorf("securitypolicyname argument is required")
+	}
+
+	log.Print("Getting policy object to modify")
+	policyToModify, err := getSingleSecurityPolicy(securityPolicyName, nsxclient)
+	log.Printf("[DEBUG] - policyTOModify :%s", policyToModify)
+
+	if err != nil {
+		return err
+	}
+
+	log.Printf(fmt.Sprintf("[DEBUG] policyToModify.Remove(%s)", name))
+	// FIXME:  RemoveFirewallActionByName probably return a error for consistency
+	policyToModify.RemoveFirewallActionByName(name)
+	log.Printf("[DEBUG] - policyTOModify :%s", policyToModify)
+	updateAPI := securitypolicy.NewUpdate(policyToModify.ObjectID, policyToModify)
+
+	err = nsxclient.Do(updateAPI)
+
+	if err != nil {
+		return fmt.Errorf("Error creating security group: %v", err)
+	}
+
+	if updateAPI.StatusCode() != 200 {
+		return fmt.Errorf("%s", updateAPI.ResponseObject())
+	}
+
+	// If we got here, the resource had existed, we deleted it and there was
+	// no error.  Notify Terraform of this fact and return successful
+	// completion.
+	d.SetId("")
+	log.Printf(fmt.Sprintf("[DEBUG] firewall rule with name %s from securitypolicy %s deleted.", name, securityPolicyName))
+
 	return nil
 }
